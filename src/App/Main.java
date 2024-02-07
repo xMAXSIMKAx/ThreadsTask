88e9d7d2a64cf5a51310f4665ac6a15a4cb3fead
xMAXSIMKAx <maxymram@gmail.com>
2024-02-07 18:31:41 +0100

    add main

--- src/App/Main.java
+++ src/App/Main.java
@@ -1,4 +1,11 @@
 package App;
 
+import App.Controler.ThreadsController;
+
 public class Main {
+    public static void main(String[] args) {
+        ThreadsController threadsController = new ThreadsController();
+        threadsController.StartThreads();
+    }
+    
 }
